40da6237ccc0ca5fbc0830952af1400d3debe58d
Sander1435 <smaekivi@gmail.com>
2025-03-08 22:52:38 +0200

    Added Feedforward control

---
+++ robot_feedforward_setup.m
@@ -0,0 +1,16 @@
+%% 1) Feedforward Disturbance Parameters (inertia, friction)
+
+% R-axis
+MR  = 0.15; 
+bvR = 0.05;    
+FcR = 0.12;    
+
+% X-axis
+MX  = 0.12;
+bvX = 0.04;
+FcX = 0.09;
+
+% Z-axis
+MZ  = 0.20;
+bvZ = 0.06;
+FcZ = 0.15;
